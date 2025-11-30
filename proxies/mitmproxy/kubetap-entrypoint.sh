f9110c52a66742e8d3d5b7efb6d09127a68cc91b
Oriano de-Stefani <ode@exigo.ch>
2025-11-30 23:42:30 +0100

    Fix shebang and improve variable quoting in kubetap-entrypoint.sh

--- proxies/mitmproxy/kubetap-entrypoint.sh
+++ proxies/mitmproxy/kubetap-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -o errexit
 set -o pipefail
@@ -9,18 +9,18 @@ set -o nounset
 # Note: We skip this if we don't have permissions, as the directory should 
 # already exist from the Dockerfile
 if [ -d /home/mitmproxy/.mitmproxy ] && [ -w /home/mitmproxy/.mitmproxy ]; then
-  # Only copy the config file if it exists and we have write access
+  # Only copy the config file if it exists and we have read access
   if [ -f /home/mitmproxy/config/config.yaml ] && [ -r /home/mitmproxy/config/config.yaml ]; then
     cp /home/mitmproxy/config/config.yaml /home/mitmproxy/.mitmproxy/config.yaml
   fi
 fi
 
-prog=${1}
-if [[ ${1} == 'mitmdump' || ${1} == 'mitmproxy' || ${1} == 'mitmweb' ]]; then
+prog="${1}"
+if [[ "${1}" == 'mitmdump' || "${1}" == 'mitmproxy' || "${1}" == 'mitmweb' ]]; then
   MITMPROXY_PATH='/home/mitmproxy/.mitmproxy'
   
   # For mitmproxy interactive terminal mode, use tmux to handle TTY requirements
-  if [[ ${1} == 'mitmproxy' ]]; then
+  if [[ "${1}" == 'mitmproxy' ]]; then
     # Start a tmux session with mitmproxy to allow interactive access without requiring a TTY
     # Users can 'kubectl exec -it <pod> -- tmux attach-session -t mitmproxy' to interact
     tmux new-session -d -s mitmproxy \; \
@@ -29,9 +29,9 @@ if [[ ${1} == 'mitmdump' || ${1} == 'mitmproxy' || ${1} == 'mitmweb' ]]; then
     # Keep the container running by attaching to the session (this allows logs to flow)
     exec tmux attach-session -t mitmproxy
   else
-    # For mitmproxy (or other commands), use direct execution
-    exec ${@} --set "confdir=${MITMPROXY_PATH}"
+    # For mitmdump or mitmweb (or other commands), use direct execution
+    exec "${@}" --set "confdir=${MITMPROXY_PATH}"
   fi
 else
-  exec ${@}
+  exec "${@}"
 fi
